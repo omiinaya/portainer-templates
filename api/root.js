c6b58175b3714be51b2eff0acf8b46568dd0879a
Omar Minaya <omiinaya@gmail.com>
2024-06-05 20:05:15 -0400

    simple web server for acquiring jsons

---
+++ api/root.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*' /*'https://econ-project.vercel.app/'*/)
+  
+  try {
+    const response = `y r u here?`
+    res.json({
+      status: 200,
+      message: response,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,66 @@
+const root = require("./api/root");
+const express = require("express");
+const app = express();
+
+const { version } = require('./package.json')
+global.AO_VERSION = version
+
+const processRequest = require('./app/process-request')
+
+function enableCORS(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Content-Encoding, Accept'
+  )
+  res.header(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PATCH, PUT, DELETE'
+  )
+  res.header('Via', `allOrigins v${version}`)
+  next()
+}
+
+app.use(express.json({ extended: false }));
+
+
+app.use("/", root);
+app.set('case sensitive routing', false)
+app.set('jsonp callback name', 'callback')
+app.disable('x-powered-by')
+app.enable("trust proxy")
+app.use(enableCORS)
+
+app.all('/:format(get|raw|json|info)', processRequest)
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
+/*                                            
+                           .'\                
+                          //  ;               
+                         /'   |               
+        .----..._    _../ |   \               
+         \'---._ `.-'      `  .'              
+          `.    '              `.     <--- dog 
+            :            _,.    '.            
+            |     ,_    (() '    |            
+            ;   .'(().  '      _/__..-        
+            \ _ '       __  _.-'--._          
+            ,'.'...____'::-'  \     `'        
+           / |   /         .---.              
+     .-.  '  '  / ,---.   (     )             
+    / /       ,' (     )---`-`-`-.._          
+   : '       /  '-`-`-`..........--'\         
+   ' :      /  /                     '.       
+   :  \    |  .'         o             \      
+    \  '  .' /          o       .       '     
+     \  `.|  :      ,    : _o--'.\      |     
+      `. /  '       ))    (   )  \>     |     
+        ;   |      ((      \ /    \___  |     
+        ;   |      _))      `'.-'. ,-'` '     
+        |    `.   ((`            |/    /      
+        \     ).  .))            '    .       
+     ----`-'-'  `''.::._________:::.'' ---                                          
+*/
