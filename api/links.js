62db54416535c24924c204b2c4f46dd90e344518
Omar Minaya <omiinaya@gmail.com>
2024-06-07 02:02:58 -0400

    .

--- api/links.js
+++ api/links.js
@@ -4,13 +4,13 @@ const list = [
     'https://raw.githubusercontent.com//technorabilia/portainer-templates/main/lsio/templates/templates-2.0.json',
     'https://raw.githubusercontent.com//Qballjos/portainer_templates/master/Template/template.json',
     'https://raw.githubusercontent.com//TheLustriVA/portainer-templates-Nov-2022-collection/main/templates_2_2_rc_2_2.json',
-    'https://raw.githubusercontent.com//ntv-one/portainer/main/template.json',
-    'https://raw.githubusercontent.com//mycroftwilde/portainer_templates/master/Template/template.json',
-    'https://raw.githubusercontent.com//mikestraney/portainer-templates/master/templates.json',
-    'https://raw.githubusercontent.com//dnburgess/self-hosted-template/master/template.json',
-    'https://raw.githubusercontent.com//SelfhostedPro/selfhosted_templates/master/Template/portainer-v2.json',
-    'https://raw.githubusercontent.com//OliverCullimore/portainer-templates/master/templates.json',
-    'https://raw.githubusercontent.com//mediadepot/templates/master/portainer.json'
+    //'https://raw.githubusercontent.com//ntv-one/portainer/main/template.json',
+    //'https://raw.githubusercontent.com//mycroftwilde/portainer_templates/master/Template/template.json',
+    //'https://raw.githubusercontent.com//mikestraney/portainer-templates/master/templates.json',
+    //'https://raw.githubusercontent.com//dnburgess/self-hosted-template/master/template.json',
+    //'https://raw.githubusercontent.com//SelfhostedPro/selfhosted_templates/master/Template/portainer-v2.json',
+    //'https://raw.githubusercontent.com//OliverCullimore/portainer-templates/master/templates.json',
+    //'https://raw.githubusercontent.com//mediadepot/templates/master/portainer.json'
 ]
 
 module.exports = list
