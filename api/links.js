6995d2a61bafc0a65f99670adcbd553452b6c297
Omar Minaya <omiinaya@gmail.com>
2024-06-07 17:47:47 -0400

    Update links.js

--- api/links.js
+++ api/links.js
@@ -11,7 +11,8 @@ const list = [
     'https://raw.githubusercontent.com/SelfhostedPro/selfhosted_templates/master/Template/portainer-v2.json',
     'https://raw.githubusercontent.com/OliverCullimore/portainer-templates/master/templates.json',
     'https://raw.githubusercontent.com/mediadepot/templates/master/portainer.json',
-    'https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/template/portainer-v2-amd64.json'
+    'https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/template/portainer-v2-amd64.json',
+    'https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json'
 ]
 
 module.exports = list
