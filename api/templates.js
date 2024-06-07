2eece386d287f47c2f132167701485f24a6a8fd5
Omar Minaya <omiinaya@gmail.com>
2024-06-07 03:23:43 -0400

    .

--- api/templates.js
+++ api/templates.js
@@ -21,15 +21,18 @@ router.get("/", async (req, res) => {
   }, []);
 
 
- // Keep the first template with each unique image or include all templates without an image
-const uniqueImages = combinedTemplatesArray.reduce((acc, cur) => {
-  if (!acc.some(t => t.image === cur.image)) {
-    acc.push(cur);
-  } else if (cur.image === undefined) {
-    acc.push(cur);
-  }
-  return acc;
-}, []);
+  const uniqueImages  = combinedTemplatesArray.reduce((acc, cur) => {
+    if (cur.image !== undefined) {  // If the template has an image
+      if (!acc.some(t => t.image === cur.image)) {  // Check for a unique combination of 'image' and 'name'
+        acc.push(cur);  // Add it to the array
+      }
+    } else {  // If the template doesn't have an image
+      if (!acc.some(t => t.name === cur.name)) {  // Check for a unique 'name'
+        acc.push(cur);  // Add it to the array
+      }
+    }
+    return acc;
+  }, []);
 
 // Create a new object with the unique templates array
 const singleObject = { version: '2', templates: uniqueImages };
